228338b7a2d34febdd01249ff69634b439eedbf1
Neha Dubey <008nehadubey@gmail.com>
2021-01-26 23:12:40 +0530

    email validation code

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ function validate() {
        return false;
     }
     if( document.myForm.EMail.value == "" ) {
-       alert( "Please provide your Email!" );
-    //    var x=document.myform.email.value;  
-    //     var atposition=x.indexOf("@");  
-    //     var dotposition=x.lastIndexOf(".");  
-    //     if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-    //     alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
-    //     return false;  
-    //     }
-    //    document.myForm.EMail.focus() ;
-       return false;
-    }
+       //alert( "Please provide your Email!" );
+        var x=document.myform.email.value;  
+        var atposition=x.indexOf("@");  
+        var dotposition=x.lastIndexOf(".");  
+        if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+        alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
+        return false;  
+        }
+     document.myForm.EMail.focus() ;
+     return false;
+    
     if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
        document.myForm.Zip.value.length != 6 ) {
        
